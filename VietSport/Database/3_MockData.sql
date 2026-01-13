5fca4a1ae4f178d926660dac0e5b8305dc920ad6
trhgbao <117840632+trhgbao@users.noreply.github.com>
2026-01-13 14:42:34 +0700

    Add files via upload

---
+++ VietSport/Database/3_MockData.sql
@@ -0,0 +1,11 @@
+﻿USE VietSportDB;
+GO
+-- Thêm chi nhánh mẫu
+INSERT INTO CoSo (MaCoSo, TenCoSo, DiaChi, ThanhPho) VALUES 
+('CS01', N'VietSport Quận 1', N'123 Nguyễn Huệ', N'Hồ Chí Minh'),
+('CS02', N'VietSport Cầu Giấy', N'456 Xuân Thủy', N'Hà Nội');
+
+-- Thêm sân mẫu
+INSERT INTO SanTheThao (MaSan, MaCoSo, LoaiSan, SucChua, TinhTrang) VALUES
+('S01', 'CS01', N'Bóng đá mini', 10, N'Còn trống'),
+('S02', 'CS01', N'Cầu lông', 4, N'Đang sử dụng');
